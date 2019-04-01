2044a25b177b3713e19feb65e3862f276869697a
yuexia <yue_xia@aliyun.com>
2019-04-01 21:03:23 +0800

    Initial commit

---
+++ src/main/java/xyz/mcyx/demo2/controller/Hello.java
@@ -0,0 +1,13 @@
+package xyz.mcyx.demo2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+}
